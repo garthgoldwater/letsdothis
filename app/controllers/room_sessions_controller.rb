d1c3bbbf6d95bd5a6b39cf249c3499ee4c9356ec
Garth Goldwater <garth.goldwater@gmail.com>
2014-08-06 13:42:43 -0400

    Extract RoomSession class

--- app/controllers/room_sessions_controller.rb
+++ app/controllers/room_sessions_controller.rb
@@ -5,36 +5,19 @@ class RoomSessionsController < ApplicationController
 
   def create
     room = Room.find(params[:room_id])
-    if logged_in?(room)
+    if room.authenticate(params[:password])
+      @room_session = RoomSession.new(session, room, params[:handle])
       redirect_to room
     else
-      if room.authenticate(params[:password])
-        log_in(params[:handle], room)
-        redirect_to room
-      else
-        flash[:warning] = "Probably you got the password wrong"
-        redirect_to [:new, room, :room_session]
-      end
+      flash[:warning] = "Probably you got the password wrong"
+      redirect_to [:new, room, :room_session]
     end
   end
 
   def destroy
     room = Room.find(params[:room_id])
-    leave(room)
+    room_session = RoomSession.new(session, room, params[:handle])
+    room_session.leave(room)
     redirect_to root_path
-  end
-
-  private
-
-  def logged_in?(room)
-    session[room.id]
-  end
-
-  def leave(room)
-    session[room.id] = nil
-  end
-
-  def log_in(handle, room)
-    session[room.id] = handle
   end
 end
